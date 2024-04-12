6ed564b859ccf7c36a9c53e3d0647f10895a5baa
kenyantequila <abdishakur431@gmail.com>
2024-04-12 18:11:54 +0300

    commit

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 // cont btn = document.getElementsByTagName('')
+fetch('http://localhost:3000/GOODS')
+.then(response => response.json())
+.then(data => {
+    data.forEach(goods =>{
+        const list = document.createElement("div");
+        dispatchEvent.className = "goods";
+        div.innerHTML = `
+        <img src"${goods.photo}"/>
+        <h3>${goods.name}</h3>
+        <p>${goods.price}</p>
+        <p>${goods.description}</p>
+        <button id ="press">BUY-NOW</button>`;
+        goods.appendChild(div);
+    
+    })
+})
+.catch(error => console.error('Error fetching data;', error));
+const press = document.getElementById('press');
+press.addEventListener("click",() => {
+    })
+    function scrollToAbout(){
+        contain.addEventListener("onclick",() => {
 
+        });
+
+    }
 // const buttons = document.querySelectorAll('.change');
 
     // Add event listener to each button
