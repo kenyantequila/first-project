fc8615a22e2c4515c8edd11a16bc4e9ac5b53872
kenyantequila <abdishakur431@gmail.com>
2024-04-13 08:52:51 +0300

    commit

--- README.md
+++ README.md
@@ -1,26 +1,14 @@
-fetch('http://localhost:3000/GOODS')
-.then(response => response.json())
-.then(data => {
-    data.forEach(goods =>{
-        const list = document.createElement("div");
-        dispatchEvent.className = "goods";
-        div.innerHTML = `
-        <img src"${goods.photo}"/>
-        <h3>${goods.name}</h3>
-        <p>${goods.price}</p>
-        <p>${goods.description}</p>
-        <button id ="press">BUY-NOW</button>`;
-        goods.appendChild(div);
-    
-    })
-})
-.catch(error => console.error('Error fetching data;', error));
-const press = document.getElementById('press');
-press.addEventListener("click",() => {
-    })
-    function scrollToAbout(){
-        contain.addEventListener("onclick",() => {
+# My first project
 
-        });
+## Date, 2024/04/13
 
-    }
+## By *Abdishukri Ally*
+
+# Description
+The code is about creating a single page application which we were require to build an API where we fetch data from your own API that you have created.The single page application is about a web application which enables customers to access the content of the mall,services and goods sold at the mall,it also gives the customers the option on how to pay for their good.
+
+# instalation
+
+
+# installation Requirments
+
--- index.js
+++ index.js
@@ -5,13 +5,13 @@ buttons.forEach(button => {
     button.addEventListener('click', function () {
 
         if (button.innerText === "Shop-now") {
-            alert("Visit our online store to explore a wide range of products!");
-            alert("dont miss the chance");
+            alert("Visit our store to explore a wide range of products!");
+            alert("dont miss the chance to shop with us not forgetting {mapema ndio best}");
         } else if (button.innerText === "Make order") {
             alert("Place your order now to enjoy fast and reliable service!");
             alert("You can make your order through our new website or you can also contact us using the contact form");
         } else if (button.innerText === "Deliveries") {
-            alert("We offer free shipping on all orders within the country!");
+            alert("We offer shipping on all orders within the country!");
             alert("We deliver to our customers doorstep without having to choose");
         }
     });
@@ -43,11 +43,21 @@ function initialize() {
 }
 
 initialize();
-// function handleSearchInput() {
-//     const searchInput = document.getElementById('searchInput').value;
-//     searchGoods(searchInput);
-// }
 
-// document.getElementById('searchInput').addEventListener('input', handleSearchInput);
 
-// initialize();
+
+document.querySelectorAll('.buttons button').forEach(button => {
+    button.addEventListener('click', function () {
+        // Your event handling code here
+        if (button.innerText == "services") {
+            alert("We offer a wide range of services to our customers.We tend to provide our customers with special services unlike  the others ,here we dont judje either poor or rich!");
+        } else if (button.innerText == "payment methods") {
+            alert("Here at Dubai shopping we have decided not to be specific with the payment method, we letb our customers choose what they like ,either using cash,pay using their phones or even cards !");
+        } else if (button.innerText == "transparency") {
+            alert
+        } else if (button.innerText == "discipline") {
+            alert("We offer a wide range of payment methods to our customers!");
+        }
+
+    });
+});
